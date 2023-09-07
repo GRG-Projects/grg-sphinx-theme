1b2337ba055117b6cd0c2df9c5c2aae1d0159559
maharshigor <gor.maharshi@gmail.com>
2023-09-07 12:05:24 -0400

    function moved to base level

--- src/grg_sphinx_theme/header.py
+++ src/grg_sphinx_theme/header.py
@@ -1,62 +1,65 @@
 from sphinx.application import Sphinx
 
+# Helper functions.
+def generate_url(link: dict, context) -> str:
+  """Generate a url to specific file if it is an internal file."""
+  if "external" in link and link["external"]:
+    return link["url"]
+  return context["pathto"](link["url"])
+
+def external_link_classes(link: dict) -> str:
+  """Required class declaration for external links."""
+  if "external" in link and link["external"]:
+    return """ nav-external"""
+  return ""
+
+def generate_basic_link(link: dict, context) -> str:
+  """Generate html code for a simple link with a tag and href populated."""
+  return f"""
+      <li class="nav-item">
+        <a class="nav-link{external_link_classes(link)}" href="{generate_url(link, context)}">
+          {link["name"]}
+        </a>
+      </li>
+      """
+
+def generate_sub_links(links: list, context) -> str:
+  """Generate html code for navigation links based upon list provided."""
+  links_html = []
+  for link in links:
+    links_html.append(generate_basic_link(link, context))
+  return "\n".join(links_html)
+
+def generate_section_title(section: str) -> str:
+  """Generate html code for navigation section title."""
+  return f"""
+      <li class="nav-item">
+        <p class="nav-section-title nav-link">
+          {section}
+        </p>
+      </li>
+      """
+
+def generate_section_wise_links(links: list, context) -> str:
+  """Generate html code for section wise navigation lists."""
+  links_html = []
+  for link in links:
+    links_html.append(generate_section_title(link["name"]))
+    links_html.append(generate_sub_links(link["children"], context))
+  return "\n".join(links_html)
+
+
+# Functions for registration in __init__.
 def add_navbar_functions(
     app: Sphinx, pagename: str, templatename: str, context, doctree
 ) -> None:
   """
-  Add functions so Jinja template can create navbar details
+  Add functions so Jinja template can create navbar details.
   """
   def generate_navbar_links() -> str:
     """
-    Generate different links for navbar_links configuration
+    Generate different links for navbar_links configuration.
     """
-
-    # returns specific path for internal files
-    def generate_url(link: dict) -> str:
-      if "external" in link and link["external"]:
-        return link["url"]
-      return context["pathto"](link["url"])
-
-    # returns the required class for external link
-    def external_link(link: dict) -> str:
-      if "external" in link and link["external"]:
-        return """ nav-external"""
-      return ""
-
-    # constructs a basic link structure
-    def generate_basic_link(link: dict) -> str:
-      return f"""
-          <li class="nav-item">
-            <a class="nav-link{external_link(link)}" href="{generate_url(link)}">
-              {link["name"]}
-            </a>
-          </li>
-          """
-
-    # constructs list of basic links
-    def generate_sub_links(links: list) -> str:
-      links_html = []
-      for link in links:
-        links_html.append(generate_basic_link(link))
-      return "\n".join(links_html)
-    
-    # constructs section title element
-    def generate_section_title(section: str) -> str:
-      return f"""
-          <li class="nav-item">
-            <p class="nav-section-title nav-link">
-              {section}
-            </p>
-          </li>
-          """
-    
-    # constructs section wise links
-    def generate_section_wise_links(links: list) -> str:
-      links_html = []
-      for link in links:
-        links_html.append(generate_section_title(link["name"]))
-        links_html.append(generate_sub_links(link["children"]))
-      return "\n".join(links_html)
     
     links = context.get("theme_navbar_links")
 
@@ -65,7 +68,7 @@ def add_navbar_functions(
     for link in links:
       # If "url" is present: direct link
       if "url" in link:
-        html_links.append(generate_basic_link(link))
+        html_links.append(generate_basic_link(link, context))
       
       # If "children" is present: simple dropdown
       elif "children" in link:
@@ -76,7 +79,7 @@ def add_navbar_functions(
                     {link["name"]}
                 </button>
                 <ul id="pst-header-nav-more-links" class="dropdown-menu">
-                    {generate_sub_links(link["children"])}
+                    {generate_sub_links(link["children"], context)}
                 </ul>
             </li>
           """
@@ -91,7 +94,7 @@ def add_navbar_functions(
                     {link["name"]}
                 </button>
                 <ul id="pst-header-nav-more-links" class="dropdown-menu">
-                    {generate_section_wise_links(link["sections"])}
+                    {generate_section_wise_links(link["sections"], context)}
                 </ul>
             </li>
           """
