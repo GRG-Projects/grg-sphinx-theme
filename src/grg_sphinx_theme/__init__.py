edfd597dc4a3357314799c079eda24cb2a14a335
Serge Koudoro <skab12@gmail.com>
2024-08-28 12:38:31 -0400

    fix: adding parallel thread safe

--- src/grg_sphinx_theme/__init__.py
+++ src/grg_sphinx_theme/__init__.py
@@ -9,14 +9,17 @@ try:
 except ImportError:
     pass
 
+
 def get_html_theme_path():
     """Return list of HTML theme paths."""
     parent = Path(__file__).parent.resolve()
     theme_path = parent / "theme" / "grg_sphinx_theme"
     return theme_path
 
+
 def setup(app):
     """Setup the Sphinx application for grg-sphinx-theme"""
     app.add_html_theme('grg_sphinx_theme', get_html_theme_path())
     app.connect("html-page-context", header.add_navbar_functions)
     app.connect("builder-inited", team.add_team_details)
+    return {"parallel_read_safe": True, "parallel_write_safe": True}
